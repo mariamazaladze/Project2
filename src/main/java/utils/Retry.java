bb20203408d71e1abfcdb3c85df262c4cfef1a5f
MAzaladze <mariamazaladze@yahoo.com>
2021-08-26 16:42:07 +0400

    1

---
+++ src/main/java/utils/Retry.java
@@ -0,0 +1,22 @@
+package utils;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+
+
+public  class Retry implements IRetryAnalyzer {
+    int retryCount  = 0;
+    int maxRetryCount=3;
+
+    @Override
+    public boolean retry(ITestResult result) {
+
+        if(retryCount<maxRetryCount){
+            retryCount++;
+            return true;
+        }
+        return false;
+    }
+
+}
